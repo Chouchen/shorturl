9ef54c068fdf26c8abfdf6c72210a3426cb4e0e9
Shikiryu <chouchen@users.noreply.github.com>
2011-01-04 10:19:57 +0000

    Bookmarklet checker

---
+++ add.php
@@ -0,0 +1,33 @@
+<? 
+header("Content-type: text/javascript");
+header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date dans le passé
+
+$s			= $_GET['s'];
+$url		= $_GET['u'];
+$title		= $_GET['t'];
+$version	= $_GET['v'];
+
+include 'class/ShortURL.php';
+
+$url = new ShortURL();
+
+if($s=='' || $url==''){
+	echo 'alert("'.ShortURL::STATE_FIELD_MISSING.'");';
+	exit;
+}
+
+$ret = $url->shortThisUrl($url, $s);
+
+if(is_bool($ret) && !$ret){
+	echo 'alert("'.ShortURL::STATE_ERROR.'");';
+	exit;
+}
+elseif($ret === ShortURL::STATE_ALREADY_EXIST){
+	echo 'alert("'.$ret.'");';
+	exit;
+}
+else{
+	echo 'alert("Raccourci cr&eacute;&eacute; : <a href="http://'.$_SERVER['SERVER_NAME'].$folder.rawurlencode($newname).'">http://'.$_SERVER['SERVER_NAME'].$folder.rawurlencode($newname).'</a>");';
+	exit;
+}
